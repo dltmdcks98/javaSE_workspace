fcd73bc9aab7f38820733147a272d78616e1b5a3
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-11 11:12:10 +0900

    08.11 Set

--- javaseapp0811/src/com/aca/javaseapp0811/test/SetTest.java
+++ javaseapp0811/src/com/aca/javaseapp0811/test/SetTest.java
@@ -1,10 +1,32 @@
 package com.aca.javaseapp0811.test;
 
-public class SetTest {
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
+public class SetTest {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Set<String> set = new HashSet<String>(); //Generic타입
+	
+		set.add("파인애플");
+		set.add("오렌지");
+		set.add("키위");
+		set.add("망고");
+		set.add("바나나");
+		
+		System.out.println("담겨진 과일의 수는 "+set.size());
+		
+		//담겨진 과일 출력, 순서 없는 집합이므로 별도의 도구를 이용하는데 
+		//이때 도구는 일렬로 늘어뜨리는 기능을 Enumeration,Iterator가 있다.
+		Iterator<String> it = set.iterator();
+		
+		while(it.hasNext()) {
+			String name = it.next();
+			System.out.println(name);
+			
+		}
 	}
 
 }
