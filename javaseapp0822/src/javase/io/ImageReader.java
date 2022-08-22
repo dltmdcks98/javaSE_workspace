afe348619237fc123ac157d75ec84c1ae673ebaf
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-22 17:38:58 +0900

    22.08.22 스트림

--- javaseapp0822/src/javase/io/ImageReader.java
+++ javaseapp0822/src/javase/io/ImageReader.java
@@ -1,5 +1,21 @@
 package javase.io;
 
-public class ImageReader {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+public class ImageReader {
+	FileInputStream fls;//파일을 대상으로 데이터를 읽어오는 바이트 기반 스트림
+	
+	public ImageReader(){
+		try {
+			fls = new FileInputStream("파일경로");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new ImageReader();
+	}
 }
