90af97e137b13ed8a43cc48d0f7688611a2d9ba2
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-25 12:17:55 +0900

    22.08.25 에코스트림 구현

--- javaseapp0825/src/javaseapp0825/network/GUClient.java
+++ javaseapp0825/src/javaseapp0825/network/GUClient.java
@@ -6,7 +6,11 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -29,6 +33,9 @@ public class GUClient extends JFrame {
 	
 	int port=9999;
 	Socket socket;
+	BufferedWriter buffw;
+	BufferedReader buffr;
+	
 	public GUClient() {
 		ch = new Choice();
 		t_port = new JTextField(Integer.toString(port),6);
@@ -40,6 +47,10 @@ public class GUClient extends JFrame {
 		
 		for(int i=13; i<=136;i++) {
 			ch.add("192.168.25."+i);
+			if(i==82) {//본인 IP인 경우
+				ch.select("192.168.25."+i);
+			}
+			
 		}
 	
 		//스타일
@@ -57,7 +68,7 @@ public class GUClient extends JFrame {
 		
 		//윈도우 설정
 		setVisible(true);
-		setSize(300,400);
+		setBounds(200,100,300,400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		//접속버튼에 리스너 연결
@@ -68,6 +79,15 @@ public class GUClient extends JFrame {
 				connect();
 			}
 		});
+		
+		//전송 버튼에 리스너 연결
+		bt_send.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				send();
+			}
+		});
 	}
 	public void connect() {
 		//원하는 서버의 IP와 포트번호를 이용해서 접속 즉 연결 시도
@@ -76,7 +96,8 @@ public class GUClient extends JFrame {
 			if(socket!=null) {
 				System.out.println("접속 성공");	
 			}
-			
+			buffw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			buffr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -84,6 +105,26 @@ public class GUClient extends JFrame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	//입력한 데이터를 서버에 보내자(출력-실행중인 프로그램내에서 보내는 것이라)
+	public void send() {
+		String msg = t_input.getText();
+		try {
+			buffw.write(msg+"\n");//버퍼로 모은 문장의 긑을 알려주기 위해 반드시 줄바꿈 문자를 포함해서 보내야 서버가 무한정 대기하지 않는다.
+			//출력스트림은 입력 스트림과는 다르게 버퍼를 처리할경우 데이터 전송시 반드시 버퍼를 비워야한다.
+			buffw.flush();//버퍼 비우기
+			t_input.setText("");//입력값 초기화
+			
+			//서버가 보낸 메시지 받기
+			String data = null;
+			data = buffr.readLine();
+			area.append(data+"\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	public static void main(String[] args) {
 		new GUClient();
--- javaseapp0825/src/javaseapp0825/network/GUIEchoServer.java
+++ javaseapp0825/src/javaseapp0825/network/GUIEchoServer.java
@@ -3,6 +3,15 @@ package javaseapp0825.network;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,13 +25,15 @@ public class GUIEchoServer extends JFrame{
 	JTextArea area;
 	JScrollPane scroll;
 	
+	ServerSocket server;//접속자 감지용 소켓
+	Thread serverThread;//서버를 가동하고 접속자를 감지하는 용도->accept()메서드는 접속자가 발생할때까지 대기상태에 빠지므로
 	public GUIEchoServer() {
-		t_port = new JTextField(12);
+		t_port = new JTextField("9999",12);
 		bt_start = new JButton("서버가동");
 		area = new JTextArea();
 		scroll = new JScrollPane(area);
 		
-		//스타일
+		//스타일 
 		scroll.setPreferredSize(new Dimension(280,320));
 		area.setBackground(Color.CYAN);
 		
@@ -34,11 +45,60 @@ public class GUIEchoServer extends JFrame{
 		
 		//윈도우 설정
 		setVisible(true);
-		setSize(300,400);
+		setBounds(500,100,300,400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+		//쓰레드를 별도의 자바로 만들어도 되지만 내부익명으로 재정의
+		serverThread = new Thread() {
+			//개발자가 쓰레드를 통해 구현하고싶은 로직은 run() 메서드에 작성해놓으면 JVM이 선택
+			public void run() {
+				startServer();
+			}
+		};
 		
+		
+		//가동버튼 리스너 연결
+		bt_start.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				serverThread.start();//개발자 주도가 아닌 JVM에게 실행을 맡긴다.
+			}
+		});
 	}
 	
+	//서버 가동하기
+	public void startServer() {
+		try {
+			server = new ServerSocket(Integer.parseInt(t_port.getText()));
+			area.append("서버 가동 및 접속자 청취충..\n");			
+			Socket socket = server.accept();//접속과 동시에 네트워크 정보 및 스트림을 제공해주는 소켓을 반환
+			String ip = socket.getInetAddress().getHostAddress();
+			area.append(ip+"접속자 발견\n" );
+			
+			//소켓으로부터 스트림을 뽑아서 데이터 주고 바기
+			BufferedReader buffr = new BufferedReader(new InputStreamReader(socket.getInputStream()));//듣기용
+			BufferedWriter buffw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));//말하기 용
+			
+			String data =null;
+			while(true) {
+				data = buffr.readLine();//한줄 읽기,한줄이 들어올때 \n도 딸려온다. 그래서 줄을 구분할 수 있음
+				area.append(data+"\n");//textarea 내에서의 줄바꿈
+				
+				//클라이언트가 보낸 메시지를 다시 돌려보내기
+				buffw.write(data+"\n");
+				buffw.flush();
+			}
+			
+			
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
 		new GUIEchoServer();
 	}
