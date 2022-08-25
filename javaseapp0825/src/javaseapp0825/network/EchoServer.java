6ebd5341b9fe9a3f54a51f9dfba0bbcc18617114
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-25 11:06:43 +0900

    22.08.25 Socket 연결

--- javaseapp0825/src/javaseapp0825/network/EchoServer.java
+++ javaseapp0825/src/javaseapp0825/network/EchoServer.java
@@ -1,7 +1,11 @@
 package javaseapp0825.network;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 //연결이 유지 StateFull , 메아리 서버 구축
 public class EchoServer {
@@ -11,6 +15,26 @@ public class EchoServer {
 	public EchoServer() {
 		try {
 			server = new ServerSocket(port);//서버생성
+			
+//			int data=-1;
+			String data =null;
+			while(true) {
+				Socket socket =server.accept();//클라이언트가 접속하기를 기다린다.
+				String ip = socket.getInetAddress().getHostAddress();
+				System.out.println("접속자 발견"+ip);
+				
+				//Socket을 통해 데이터를 클라이언트와 주고 받을 수 있다. 이때 개발자는 네트워크에 대한 지식이 필요없고 IO(스트림)제어에 집중
+				//가능한 이유: 소켓이 네트워크 하부 구조에 대한 추상화 담당
+				InputStream is =socket.getInputStream();
+				InputStreamReader reader=new InputStreamReader(is);//바이트->문자기반
+				BufferedReader buffr = new BufferedReader(reader);
+				
+//				data=is.read();//1byte읽기
+				data = buffr.readLine();
+				System.out.println(data);
+			}
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
